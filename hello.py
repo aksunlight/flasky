79ac1f039ec8a29eff74df24f27149df4e91828c
marst <marst>
2022-05-22 13:57:03 +0800

    添加文件

--- hello.py
+++ hello.py
@@ -1,17 +1,18 @@
-from flask import Flask, request
+from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
 manager = Manager(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Hello World! Your browser is %s</p>' % user_agent
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__=='__main__':
     manager.run()
